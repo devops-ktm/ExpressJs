c7c5a3304a6893691327634817e871b7ed58b8f4
sunil sharma <129640411+sunil1289@users.noreply.github.com>
2025-07-03 13:49:11 +0545

    inital commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const fs =require('fs')
+const app = express ();
+const users = require("./MOCK_DATA.json");
+const PORT = 8000;
+
+//middleware
+app.use(express.urlencoded({extended: false}));
+//routes
+app.use(express.json()); // Add this line above your routes
+
+app.get("/api/users",(req,res) =>{
+    return res.json(users);
+})
+app.get("/users",(req,res) =>{
+    const html =`
+  <ul>
+${users.map(user => `<li>${user.first_name}</li>`).join("")}
+  </ul> 
+`;
+res.send(html);
+});
+
+app.route("/api/users/:id").get((req,res) =>{
+   const id = Number(req.params.id);
+   const user = users.find((user) => user.id === id);
+   return res.json(user);
+
+
+}).put((req,res)=>{
+    //edit user with id
+    res.json({status : "pending"});
+})
+.delete((req,res) => {
+    //delete user with id
+     res.json({status : "pending"})
+});
+
+
+app.post("/api/users",(req,res) =>{
+    const body =req.body;
+    users.push({ ...body, id: users.length + 1});
+        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data) =>{
+            return res.json({status: "sucess", id: users.length + 1});
+        });
+});
+
+
+app.listen(PORT, () => console.log(`server started at PORT:${PORT}`))
+
+
+
+
